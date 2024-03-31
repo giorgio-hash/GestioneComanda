a5217644d16ef558062533cfffac189693efeb8e
giorgio-hash <g.chirico1@studenti.unibg.it>
2024-03-31 18:45:54 +0200

    dettagliato ulteriormente la struttura
    creato e provato ambiente kafka+zookeeper
    
    iniziato a scrivere gli adapter per message broker (sia pub che sub). da testare

--- src/main/java/com/example/gestionecomanda/Infrastructure/MessageBroker/CucinaPubAdapter.java
+++ src/main/java/com/example/gestionecomanda/Infrastructure/MessageBroker/CucinaPubAdapter.java
@@ -3,15 +3,18 @@ package com.example.gestionecomanda.Infrastructure.MessageBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
 
+@Service
 public class CucinaPubAdapter {
 
+    @Autowired
     private KafkaTemplate<String,Object> template;
 
     public void sendMessageToTopic(String message){
-        CompletableFuture<SendResult<String, Object>> future = (CompletableFuture<SendResult<String, Object>>) template.send("cucina-demo-1", message);
+        CompletableFuture<SendResult<String, Object>> future = template.send("cucina-demo-1", message).completable();
         future.whenComplete((result,ex)->{
             if(ex == null){
                 System.out.println("Sent Message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
