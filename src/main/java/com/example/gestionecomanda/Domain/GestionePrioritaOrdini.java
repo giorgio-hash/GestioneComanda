89da49a27adc932eb745f0f1604d4d8c60e328f7
giorgio-hash <g.chirico1@studenti.unibg.it>
2024-04-15 19:04:45 +0200

    aggiunto datasource MariaDB e collegato JPA
    in Domain:
    - aggiunti DTO
    - aggiunti Repository
    
    in Configuration:
    - aggiunta classe InfrastructureConfiguration
    - aggiunta classe per configurare JPADBAdapter
    
    implementato il JPADBAdapter
    
    aggiunte dipendenze nel pom.xml
    
    aggiunto database
    
    modificato application.properties sia per build che per test

--- src/main/java/com/example/gestionecomanda/Domain/GestionePrioritaOrdini.java
+++ src/main/java/com/example/gestionecomanda/Domain/GestionePrioritaOrdini.java
@@ -1,4 +1,13 @@
 package com.example.gestionecomanda.Domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GestionePrioritaOrdini {
+
+    @Autowired
+    @Qualifier("JPADBAdapter")
+    private DataPort dataPort;
 }
