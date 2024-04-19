4ac152f666d40b394050ddc34e346683edfcd663
giorgio-hash <g.chirico1@studenti.unibg.it>
2024-04-19 19:06:52 +0200

    Revert "errore"
    This reverts commit 731d35d2cb2ee603530e5cad54b48d0adf255d3b.

--- src/main/java/com/example/gestionecomanda/Domain/Repository/OrdineEntity2Repository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gestionecomanda.Domain.Repository;
-
-import com.example.gestionecomanda.Domain.Entity.OrdineEntity2;
-import com.example.gestionecomanda.Domain.Entity.OrdineEntity2PK;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrdineEntity2Repository extends CrudRepository<OrdineEntity2, OrdineEntity2PK> {
-}
